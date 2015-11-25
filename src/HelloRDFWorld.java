9690b5adff282a30f11b06ccc7cf7159bd2f30c4
Lennart <bout0036@hz.nl>
2015-11-25 15:45:47 +0100

    Added the right imports

--- src/HelloRDFWorld.java
+++ src/HelloRDFWorld.java
@@ -1,3 +1,8 @@
+import org.apache.jena.datatypes.xsd.XSDDatatype;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.Resource;
 
 /**
  * @author Carlo Geertse
